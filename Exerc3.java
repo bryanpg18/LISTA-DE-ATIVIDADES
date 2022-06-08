b9a658c7e839a03b6db85e037803016f5ecfa470
Bryan Pereira <107115383+bryanpg18@users.noreply.github.com>
2022-06-08 09:02:26 -0300

    Add files via upload

---
+++ Exerc3.java
@@ -0,0 +1,16 @@
+package Exerc3;
+import java.util.Scanner;
+public class Ativ_03 {
+public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        Float a, b, c, d;
+        a = (3 - 2 - 1 + 2 + 1 + 3f);
+        System.out.println("Resultado de A: " + a);
+        b = (2 * 3 - 4 * 5f);
+        System.out.println("Resultado de B: " + b);
+        c = (2 + 6 - 3)/7 * 9f;
+        System.out.println("Resultado de C: " + c);
+        d = (3 * 4 / 100 - 8f);
+        System.out.println("Resultado de D:" + d);
+        }
+}
